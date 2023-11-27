ef8ad5c1b76e3e21114568d73c8d6a03349065fe
Yohannes <yohannes.gobezie@gmail.com>
2023-11-26 17:49:21 -0700

    Update Tasks.tsx

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -4,9 +4,11 @@ import {FaRegTrashAlt} from 'react-icons/fa'
 const style = {
   li: 'flex justify-between bg-slate-200 p-4 my-2 capitalize',
   li_complete: 'flex justify-between bg-slate-400 p-4 my-2 capitalize',
-  row: 'flex',
-  text: 'ml-2 cursor-pointer',
-  text_complete: 'ml-2 cursor-pointer line-through',
+  row: 'flex-col',
+  title: 'ml-2 text-xl font-bold text-gray-800',
+  text: 'ml-2',
+  text_complete: 'ml-2 line-through',
+  date: 'ml-2',
   button: 'cursor-pointer flex items-center'
 }
 
@@ -14,14 +16,14 @@ const Tasks = ({task, toggleComplete, deleteTask}) => {
   return (
     <li className = {task.completed ? style.li_complete : style.li}>
       <div className={style.row}>
-        <input onChange={() => toggleComplete(task)} type='checkbox' checked={task.completed ? 'checked' : ''} />
-        <h1 onClick={() => toggleComplete(task)} className={task.completed ? style.text_complete : style.text}>{task.title}</h1>
+        {/* <input onChange={() => toggleComplete(task)} type='checkbox' checked={task.completed ? 'checked' : ''} /> */}
+        <h1 className={task.completed ? style.text_complete : style.title}>{task.title}</h1>
         <p className={task.completed ? style.text_complete : style.text}>{task.desc}</p>
-        <p className={task.completed ? style.text_complete : style.text}>{task.date}</p>
+        <p className={task.completed ? style.text_complete : style.date}>Due: {task.date}</p>
         <p className={task.completed ? style.text_complete : style.text}>{task.priority}</p>
         <p className={task.completed ? style.text_complete : style.text}>{task.status}</p>
       </div>
-      <button onClick={() => deleteTask(task.id)}>{<FaRegTrashAlt />}</button>
+      <button className={style.button} onClick={() => deleteTask(task.id)}>{<FaRegTrashAlt />}</button>
     </li>
   );
 };
